91206d143fc0203e63bf9f75d43eff0962c4e5de
vulrandier <dittrich.franz@gmx.de>
2021-03-11 14:05:56 +0100

    added esxi online state check

---
+++ collectors/EsxiOnlineStateCollector.py
@@ -0,0 +1,33 @@
+import logging
+from os import getenv
+
+from prometheus_client.core import GaugeMetricFamily
+
+import modules.TimedBlacklist as blacklist
+from BaseCollector import BaseCollector
+
+# init logging
+logger = logging.getLogger('esxi-exporter')
+
+
+class EsxiOnlineStateCollector(BaseCollector):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def collect(self):
+
+        gauge_metric = GaugeMetricFamily('esxi_online_state', '1=online, 0=offline',
+                                         labels=['vcenter', 'host', 'origin'])
+
+        # get all hosts from vcenter
+        vc_hosts = self.get_vcenter_hosts()
+        for host in vc_hosts:
+            vc_state = host.overallStatus == 'green'
+            gauge_metric.add_metric(labels=[getenv('vcenter_url'), host.name, 'vcenter'], value=vc_state)
+
+            nb_state = self.netbox.is_host_active(host)
+            gauge_metric.add_metric(labels=[getenv('vcenter_url'), host.name, 'netbox'], value=nb_state)
+
+            bl_state = blacklist.is_host_allowed(host.name)
+            gauge_metric.add_metric(labels=[getenv('vcenter_url'), host.name, 'ssh_connection'],value=bl_state)
