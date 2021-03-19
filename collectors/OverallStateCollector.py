8a87b8c4e13dcaafcef5c6c98609120c11c7420d
vulrandier <dittrich.franz@gmx.de>
2021-03-19 10:41:08 +0100

    refactoring - not finished yet

---
+++ collectors/OverallStateCollector.py
@@ -0,0 +1,11 @@
+from BaseCollector import BaseCollector
+
+
+class EsxiOverallStateCollector(BaseCollector):
+
+    def describe(self):
+        pass
+    
+    def collect(self):
+        pass
+   
